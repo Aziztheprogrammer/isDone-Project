fbf79230d66360d6034bd6436baac2d4c5a5d77b
Aziz Berhouma <aziz.theprogrammer@gmail.com>
2023-10-12 20:02:21 +0200

    Developing The Create Task Popup

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-// Burger Menu Close / Open
-let menuIcon = document.getElementById("menu-icon");
-let links = document.querySelector("header .container nav ul");
-let closeMenuIcon = document.getElementById("close-menu-icon");
+// // Burger Menu Close / Open
+// let menuIcon = document.getElementById("menu-icon");
+// let links = document.querySelector("header .container nav ul");
+// let closeMenuIcon = document.getElementById("close-menu-icon");
 
-menuIcon.addEventListener("click", function (i) {
-	links.style.display = "flex";
-	closeMenuIcon.style.display = "block";
-}); // Open
+// menuIcon.addEventListener("click", function (i) {
+// 	links.style.display = "flex";
+// 	closeMenuIcon.style.display = "block";
+// }); // Open
 
-closeMenuIcon.addEventListener("click", function (i) {
-	links.style.display = "none";
-	closeMenuIcon.style.display = "none";
-}); // Close
+// closeMenuIcon.addEventListener("click", function (i) {
+// 	links.style.display = "none";
+// 	closeMenuIcon.style.display = "none";
+// }); // Close
 
-window.addEventListener("resize", function (e) {
-	if (window.innerWidth <= 769) {
-		links.style.display = "none";
-	} else {
-		links.style.display = "flex"
-	}
+// window.addEventListener("resize", function (e) {
+// 	if (window.innerWidth <= 769) {
+// 		links.style.display = "none";
+// 	} else {
+// 		links.style.display = "flex"
+// 	}
+// });
+
+// Create New Task Popup
+let popButton = document.querySelector("div.add-task");
+let addTaskPopup = document.querySelector("div.task-pop");
+popButton.addEventListener("click", function (e) {
+	addTaskPopup.style.display = "flex";
+	addTaskPopup.style.animationName = "popup-opacity";
+}); // Open Popup
+
+let closePopup = window.getComputedStyle(
+    document.querySelector(".task-pop"), '::after'
+);
+
+closePopup.addEventListener("click", function (e) {
+	addTaskPopup.style.display = "none";
 });
