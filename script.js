1628f012477f40fc7ef1000ad9496405a171279c
Aziz Berhouma <aziz.theprogrammer@gmail.com>
2023-10-14 16:37:09 +0200

    Finishing Sending Data To Local Storage + Loading Tasks In The Main Tasks Page

--- script.js
+++ script.js
@@ -24,17 +24,26 @@ window.addEventListener("resize", function (e) {
 // Create New Task Popup
 let popButton = document.querySelector("div.add-task");
 let addTaskPopup = document.querySelector("div.task-pop");
+
+function openPopup(button) { // Open Popup
+	addTaskPopup.style.display = "flex";
+	addTaskPopup.style.animationName = "popup-opacity-open";
+	popButton.classList.replace("false", "true"); 
+}
+
+function closePopup(button) { // Close Popup
+	addTaskPopup.style.animationName = "popup-opacity-close";
+	setTimeout(function () {
+		addTaskPopup.style.display = "none";
+	}, 1000);
+	popButton.classList.replace("true", "false");
+}
+
 popButton.addEventListener("click", function (e) {
 	if (this.classList.contains("false")) {
-		addTaskPopup.style.display = "flex";
-		addTaskPopup.style.animationName = "popup-opacity-open";
-		this.classList.replace("false", "true"); // Open Popup
+		openPopup(this);
 	} else {
-		addTaskPopup.style.animationName = "popup-opacity-close";
-		setTimeout(function () {
-			addTaskPopup.style.display = "none";
-		}, 1000);
-		this.classList.replace("true", "false"); // Close Popup
+		closePopup(this);
 	}
 });
 
@@ -51,10 +60,12 @@ taskNameInput.addEventListener("blur", function (e) {
 	}
 });
 
+createTaskButton.addEventListener("click", function (e) {
+	closePopup(e.target);
+	window.location.reload();
+})
 
 // Send Task Name And Type To LocalStorage
-let tasks = [];
-
 class Task {
 	constructor(name, type) {
 		this.name = name;
@@ -62,9 +73,72 @@ class Task {
 	}
 }
 
+let types = document.querySelectorAll(".task-type > input");
+
+function taskType() {
+
+let type; 
+
+	types.forEach(function (t) {
+		if (t.checked) {
+			switch (t.id) {
+			case "personal-type":
+				type = "Personal";
+				break;
+			case "family-type":
+				type = "Family";
+				break;
+			case "work-type":
+				type = "Work";
+				break;
+			default:
+				type = "Not Chosen";
+			}
+		}
+	});
+
+	return type;
+}
+
 createTaskButton.addEventListener("click", function (e) {
-	tasks.unshift(new Task(taskNameInput.value, null));
-	localStorage.setItem(`Tasks ${tasks.length}`, JSON.stringify(tasks[0]));
+	localStorage.setItem(`Task ${localStorage.length + 1}`, 
+		JSON.stringify(new Task(taskNameInput.value, taskType())));
 
 });
+
+// Load Tasks
+
+let tasksList = document.getElementsByClassName("tasks-list")[0];
+
+window.onload = function () {
+	for (let i = 0; i < localStorage.length; i++) {
+		// Create Parent Task Div
+		let task = document.createElement("div");
+		task.classList.add("task", "pending");
+
+		// Create Task Info Div (Name + Type)
+		let taskInfo = document.createElement("div");
+		taskInfo.className = "task-info";
+		let taskName = document.createElement("h2");
+		taskName.textContent = JSON.parse(localStorage.getItem(localStorage.key(i))).name;
+		taskInfo.append(taskName);
+		let span1 = document.createElement("span");
+		span1.textContent = "Type : "
+		taskInfo.append(span1);
+		let taskTypeSpan = document.createElement("span");
+		taskTypeSpan.textContent = JSON.parse(localStorage.getItem(localStorage.key(i))).type;
+		span1.append(taskTypeSpan);
+
+		// Create Status Sign
+		let taskStatus = document.createElement("span");
+		taskStatus.className = "task-status";
+
+		// Create The Full Task
+		task.append(taskInfo);
+		task.append(taskStatus);
+
+		// Append The Task To The List
+		tasksList.append(task);
+	}
+};
 
